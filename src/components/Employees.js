0a68b72744693561496149c118536c7adba70e5f
Elvin Baxsh <47717988+elvinbaxsh28@users.noreply.github.com>
2022-11-13 02:27:08 +0400

    Add js

---
+++ src/components/Employees.js
@@ -0,0 +1,16 @@
+const Employees = [
+    {
+        id:"1",
+        Name:"Elvin Baxsh",
+        Age:"22"
+    },{
+        id:"2",
+        Name:"Huseyn Fogito",
+        Age:"30"
+    },{
+        id:"3",
+        Name:"Senan Senior",
+        Age:"28"
+    }
+]
+export default Employees;
